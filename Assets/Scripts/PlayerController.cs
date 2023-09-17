28c8ffc1ec223e452fc31749e323bbfeb3df6b21
PlaPud <patcub252545@gmail.com>
2023-09-17 11:51:01 +0700

    Player Sliding

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -29,12 +29,15 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D _playerRigidBody;
     private Animator _playerAnimator;
     private float walkInput = 0f;
+
     private bool _isCrouch = false;
     private bool _isWallSliding = false;
     private bool _isSprinting = false;
     private bool _isJump = false;
+    private bool _isGrounded = false;
+    private bool _isSpaceAbove = true;
 
-    private RaycastHit2D _wallCheckhit;
+    private RaycastHit2D _isTouchWall;
 
     // Start is called before the first frame update
     void Start()
@@ -47,9 +50,17 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         walkInput = Input.GetAxis("Horizontal");
-        _isJump = Input.GetKeyDown(KeyCode.Space) && isGrounded();
+        _isJump = Input.GetKeyDown(KeyCode.Space) && _isGrounded;
         _isSprinting = Input.GetKey(KeyCode.LeftShift) && !_isCrouch; 
-        
+
+        _isGrounded = Physics2D.BoxCast(
+                transform.position, boxSize, 0,
+                -transform.up, castDistance, groundLayer
+           );
+        _isSpaceAbove = !Physics2D.BoxCast(
+                transform.position, boxSizeAbove, 0,
+                transform.up, aboveCastDistance, groundLayer
+            );
         _playerRigidBody.velocity = new Vector2(
                 walkInput * (
                 _isSprinting ? 
@@ -59,6 +70,7 @@ public class PlayerController : MonoBehaviour
                 : walkSpeed),
                 _isJump ? jumpSpeed : _playerRigidBody.velocity.y
             );
+
         _playerAnimator.SetFloat("walkSpeed", Mathf.Abs(_playerRigidBody.velocity.x));
         _playerAnimator.SetFloat("jumpSpeed", _playerRigidBody.velocity.y);
         
@@ -74,7 +86,7 @@ public class PlayerController : MonoBehaviour
     {
         bool isFacingRight = transform.localScale.x == 1;
 
-        _wallCheckhit = Physics2D.Raycast(
+        _isTouchWall = Physics2D.Raycast(
                 transform.position,
                 new Vector2(
                         isFacingRight ? 
@@ -85,34 +97,33 @@ public class PlayerController : MonoBehaviour
             );
         Debug.DrawRay(transform.position, new Vector2(wallDistance, 0), Color.blue);
 
-        if (_wallCheckhit && !isGrounded() && Mathf.Abs(walkInput) > 0.5f)
+        if (_isTouchWall && 
+            !_isGrounded && 
+            _playerRigidBody.velocity.y < -0.5 && 
+            Mathf.Abs(walkInput) > 0.5f)
         {
             _isWallSliding = true;
-            jumpTime = Time.time + wallJumpTime;
         }
-        else if (jumpTime < Time.time) 
+        else
         {
             _isWallSliding = false;
         }
 
-        if (_isWallSliding) 
+        if (_isWallSliding && _playerRigidBody.velocity.y < -wallSlideSpeed) 
         {
             _playerRigidBody.velocity = new Vector2(
                     _playerRigidBody.velocity.x,
-                    Mathf.Clamp(
-                            _playerRigidBody.velocity.y,
-                            wallSlideSpeed,
-                            float.MaxValue 
-                        )
+                    -wallSlideSpeed
                 );
         }
+        _playerAnimator.SetBool("isWallSlide", _isWallSliding);
     }
 
     private void HandleCrouching() 
     {
         if (!_isJump && !_isSprinting && Input.GetKeyDown(KeyCode.C))
         {
-            if (_isCrouch && !isSpaceAbove()) return;
+            if (_isCrouch && !_isSpaceAbove) return;
             _isCrouch = !_isCrouch;
             _playerAnimator.SetBool("isCrouch", _isCrouch);
         }
@@ -130,35 +141,6 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private bool isGrounded() 
-    {
-        if (Physics2D.BoxCast(
-                transform.position, boxSize, 0,
-                -transform.up, castDistance, groundLayer
-           ))
-        {
-            return true;
-        }
-        else 
-        {
-            return false;
-        }
-    }
-
-    private bool isSpaceAbove() 
-    {
-        if (Physics2D.BoxCast(
-                transform.position, boxSizeAbove, 0,
-                transform.up, aboveCastDistance, groundLayer
-            ))
-        {
-            return false;
-        }
-        else 
-        {
-            return true;
-        }
-    }
 
     private void OnDrawGizmos()
     {
